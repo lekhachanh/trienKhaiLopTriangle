4ef859930c93dc4a346ccdc448a227019830ec61
lekhachanh <lekhachanh123@gmail.com>
2019-07-10 21:33:31 +0700

    commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        Shape shape = new Shape("blue");
+        System.out.println(shape);
+
+        Triangle triangle = new Triangle("white", 3.0, 4.0, 5.0 );
+        System.out.println(triangle);
+    }
+
+}
